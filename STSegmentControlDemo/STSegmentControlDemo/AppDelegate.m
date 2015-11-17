1bb17624afacd9a194c719fcc7b990ce0cdb26d0
shenzhaoliang <409178030@qq.com>
2015-11-17 14:52:48 +0800

    01、创建文件

---
+++ STSegmentControlDemo/STSegmentControlDemo/AppDelegate.m
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.m
+//  STSegmentControlDemo
+//
+//  Created by shentian on 15/11/17.
+//  Copyright © 2015年 ST. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    return YES;
+}
+@end
---
+++ STSegmentControlDemo/STSegmentControlDemo/STSegmentControl.h
@@ -0,0 +1,13 @@
+//
+//  STSegmentControl.h
+//  STSegmentControlDemo
+//
+//  Created by shentian on 15/11/17.
+//  Copyright © 2015年 ST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STSegmentControl : UIView
+
+@end
